2a1de339d8a553797915fa1365a3ea8653210cd7
Manoj M S <163495384+Manojms2004@users.noreply.github.com>
2025-08-08 11:34:55 +0530

    Spotify app first commit made all changes done

---
+++ spo.js
@@ -0,0 +1,298 @@
+function Arjith_song()
+{
+    var audio  =document.getElementById("ar-audio_arjit");
+    audio.play();
+    document.getElementById("arjit").style.display = "none";
+    document.getElementById("row_1_c1").style.display = "block";
+}
+function stop_song_Arjit()
+{
+    var audio  =document.getElementById("ar-audio_arjit");
+    audio.pause();
+    document.getElementById("arjit").style.display = "block";
+    document.getElementById("row_1_c1").style.display = "none";
+}
+function Neha_song()
+{
+    var audio  =document.getElementById("ar-audio_Neha");
+    audio.play();
+    document.getElementById("Neha").style.display = "none";
+    document.getElementById("row_1_c2").style.display = "block";
+}
+function stop_song_Neha()
+{
+    var audio  =document.getElementById("ar-audio_Neha");
+    audio.pause();
+    document.getElementById("Neha").style.display = "block";
+    document.getElementById("row_1_c2").style.display = "none";
+}
+function Anirud_song()
+{
+    var audio  =document.getElementById("ar-audio_Anirud");
+    audio.play();
+    document.getElementById("anir").style.display = "none";
+    document.getElementById("row_1_c3").style.display = "block";
+}
+function stop_song_Anirud()
+{
+    var audio  =document.getElementById("ar-audio_Anirud");
+    audio.pause();
+    document.getElementById("anir").style.display = "block";
+    document.getElementById("row_1_c3").style.display = "none";
+}
+function pritam_song()
+{
+    var audio  =document.getElementById("ar-audio_pritam");
+    audio.play();
+    document.getElementById("pritam").style.display = "none";
+    document.getElementById("row_1_c4").style.display = "block";
+}
+function stop_song_pritam()
+{
+    var audio  =document.getElementById("ar-audio_pritam");
+    audio.pause();
+    document.getElementById("pritam").style.display = "block";
+    document.getElementById("row_1_c4").style.display = "none";
+}
+function shreya_song()
+{
+    var audio  =document.getElementById("ar-audio_shreya");
+    audio.play();
+    document.getElementById("shreya").style.display = "none";
+    document.getElementById("row_1_c5").style.display = "block";
+}
+function stop_song_shreya()
+{
+    var audio  =document.getElementById("ar-audio_shreya");
+    audio.pause();
+    document.getElementById("shreya").style.display = "block";
+    document.getElementById("row_1_c5").style.display = "none";
+}
+function hemanth_song()
+{
+    var audio  =document.getElementById("ar-audio_hemanth");
+    audio.play();
+    document.getElementById("hemanth").style.display = "none";
+    document.getElementById("row_1_c6").style.display = "block";
+}
+function stop_song_hemanth()
+{
+    var audio  =document.getElementById("ar-audio_hemanth");
+    audio.pause();
+    document.getElementById("hemanth").style.display = "block";
+    document.getElementById("row_1_c6").style.display = "none";
+}
+function raghu_song()
+{
+    var audio  =document.getElementById("ar-audio_raghu");
+    audio.play();
+    document.getElementById("raghu").style.display = "none";
+    document.getElementById("row_1_c7").style.display = "block";
+}
+function stop_song_raghu()
+{
+    var audio  =document.getElementById("ar-audio_raghu");
+    audio.pause();
+    document.getElementById("raghu").style.display = "block";
+    document.getElementById("row_1_c7").style.display = "none";
+}
+function sanjit_song()
+{
+    var audio  =document.getElementById("ar-audio_sanjit");
+    audio.play();
+    document.getElementById("sanjit").style.display = "none";
+    document.getElementById("row_1_c8").style.display = "block";
+}
+function stop_song_sanjit()
+{
+    var audio  =document.getElementById("ar-audio_sanjit");
+    audio.pause();
+    document.getElementById("sanjit").style.display = "block";
+    document.getElementById("row_1_c8").style.display = "none";
+}
+function vijay_song()
+{
+    var audio  =document.getElementById("ar-audio_vijay");
+    audio.play();
+    document.getElementById("vijay").style.display = "none";
+    document.getElementById("row_1_c9").style.display = "block";
+}
+function stop_song_vijay()
+{
+    var audio  =document.getElementById("ar-audio_vijay");
+    audio.pause();
+    document.getElementById("vijay").style.display = "block";
+    document.getElementById("row_1_c9").style.display = "none";
+}
+function Anuraga_song()
+{
+    var audio  =document.getElementById("ar-audio_Anuraga");
+    audio.play();
+    document.getElementById("Anuraga").style.display = "none";
+    document.getElementById("row_1_c10").style.display = "block";
+}
+function stop_song_Anuraga()
+{
+    var audio  =document.getElementById("ar-audio_Anuraga");
+    audio.pause();
+    document.getElementById("Anuraga").style.display = "block";
+    document.getElementById("row_1_c10").style.display = "none";
+}
+function guru_song()
+{
+    var audio  =document.getElementById("ar-audio_guru");
+    audio.play();
+    document.getElementById("guru").style.display = "none";
+    document.getElementById("row_1_c11").style.display = "block";
+}
+function stop_song_guru()
+{
+    var audio  =document.getElementById("ar-audio_guru");
+    audio.pause();
+    document.getElementById("guru").style.display = "block";
+    document.getElementById("row_1_c11").style.display = "none";
+}
+function sp_song()
+{
+    var audio  =document.getElementById("ar-audio_sp");
+    audio.play();
+    document.getElementById("sp").style.display = "none";
+    document.getElementById("row_1_c12").style.display = "block";
+}
+function stop_song_sp()
+{
+    var audio  =document.getElementById("ar-audio_sp");
+    audio.pause();
+    document.getElementById("sp").style.display = "block";
+    document.getElementById("row_1_c12").style.display = "none";
+}
+function rajesh_song()
+{
+    var audio  =document.getElementById("ar-audio_rajesh");
+    audio.play();
+    document.getElementById("rajesh").style.display = "none";
+    document.getElementById("row_1_c13").style.display = "block";
+}
+function stop_song_rajesh()
+{
+    var audio  =document.getElementById("ar-audio_rajesh");
+    audio.pause();
+    document.getElementById("rajesh").style.display = "block";
+    document.getElementById("row_1_c13").style.display = "none";
+}
+function sonu_song()
+{
+    var audio  =document.getElementById("ar-audio_sonu");
+    audio.play();
+    document.getElementById("sonu").style.display = "none";
+    document.getElementById("row_1_c14").style.display = "block";
+}
+function stop_song_sonu()
+{
+    var audio  =document.getElementById("ar-audio_sonu");
+    audio.pause();
+    document.getElementById("sonu").style.display = "block";
+    document.getElementById("row_1_c14").style.display = "none";
+}
+function puneeth_song()
+{
+    var audio  =document.getElementById("ar-audio_puneeth");
+    audio.play();
+    document.getElementById("puneeth").style.display = "none";
+    document.getElementById("row_1_c15").style.display = "block";
+}
+function stop_song_puneeth()
+{
+    var audio  =document.getElementById("ar-audio_puneeth");
+    audio.pause();
+    document.getElementById("puneeth").style.display = "block";
+    document.getElementById("row_1_c15").style.display = "none";
+}
+function chaithra_song()
+{
+    var audio  =document.getElementById("ar-audio_chaithra");
+    audio.play();
+    document.getElementById("chaithra").style.display = "none";
+    document.getElementById("row_1_c16").style.display = "block";
+}
+function stop_song_chaithra()
+{
+    var audio  =document.getElementById("ar-audio_chaithra");
+    audio.pause();
+    document.getElementById("chaithra").style.display = "block";
+    document.getElementById("row_1_c16").style.display = "none";
+}
+function latha_song()
+{
+    var audio  =document.getElementById("ar-audio_latha");
+    audio.play();
+    document.getElementById("latha").style.display = "none";
+    document.getElementById("row_1_c17").style.display = "block";
+}
+function stop_song_latha()
+{
+    var audio  =document.getElementById("ar-audio_latha");
+    audio.pause();
+    document.getElementById("latha").style.display = "block";
+    document.getElementById("row_1_c17").style.display = "none";
+}
+function shastri_song()
+{
+    var audio  =document.getElementById("ar-audio_shastri");
+    audio.play();
+    document.getElementById("shastri").style.display = "none";
+    document.getElementById("row_1_c18").style.display = "block";
+}
+function stop_song_shastri()
+{
+    var audio  =document.getElementById("ar-audio_shastri");
+    audio.pause();
+    document.getElementById("shastri").style.display = "block";
+    document.getElementById("row_1_c18").style.display = "none";
+}
+function rajkumar_song()
+{
+    var audio  =document.getElementById("ar-audio_rajkumar");
+    audio.play();
+    document.getElementById("rajkumar").style.display = "none";
+    document.getElementById("row_1_c19").style.display = "block";
+}
+function stop_song_rajkumar()
+{
+    var audio  =document.getElementById("ar-audio_rajkumar");
+    audio.pause();
+    document.getElementById("rajkumar").style.display = "block";
+    document.getElementById("row_1_c19").style.display = "none";
+}
+function janaki_song()
+{
+    var audio  =document.getElementById("ar-audio_janaki");
+    audio.play();
+    document.getElementById("janaki").style.display = "none";
+    document.getElementById("row_1_c20").style.display = "block";
+}
+function stop_song_janaki()
+{
+    var audio  =document.getElementById("ar-audio_janaki");
+    audio.pause();
+    document.getElementById("janaki").style.display = "block";
+    document.getElementById("row_1_c20").style.display = "none";
+}
+function instIcon()
+{
+    var hlink = document.getElementById
+}
+function instaclick()
+{
+    window.location = "https://www.instagram.com/spotify/"
+}
+function twitter()
+{
+    window.location = "https://twitter.com/spotify"
+}
+function faceBook()
+{
+    window.location = "https://www.facebook.com/Spotify"
+}
+
+
